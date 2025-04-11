dfa173da7630074cbe13819e2cedaed651573bdc
Devin Judson <djudson3497@gmail.com>
2025-04-10 21:35:17 -0400

    Module 6 completed baseline

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -52,7 +52,70 @@ const tripsFindByCode = async (req, res) => {
 
 };
 
+//POST: /trips/:tripCode -lists a signle trip
+// Regardless of outcome, response must incluse HTML status code
+// and JSON message to the requesting client
+const tripsAddTrip = async (req, res) => {
+    try {
+        const trip = await Model.create({
+            code: req.body.code,
+            name: req.body.name,
+            length: req.body.length,
+            start: req.body.start,
+            resort: req.body.resort,
+            perPerson: req.body.perPerson,
+            image: req.body.image,
+            description: req.body.description,
+        });
+        return res.status(201).json(trip); // Successfully created
+    } catch (err) {
+        return res.status(400).json(err); // Handle errors
+    }
+};
+
+// PUT: /trips/:tripCode -Adds a new Trip
+// Regardless of outcome, response must incluse HTML status code
+// and JSON message to the requesting client
+const tripsUpdateTrip = async (req, res) => {
+    //uncomment for debugging
+    // console.log(req.params);
+    //console.log(req.body);
+
+    const q = await Model
+        .findOneAndUpdate(
+            {'code' : req.params.tripCode}, 
+            {
+                code: req.body.code,
+                name: req.body.name,
+                length: req.body.length,
+                start: req.body.start,
+                resort: req.body.resort,
+                perPerson: req.body.perPerson,
+                image: req.body.image,
+                description: req.body.description,
+            },
+        )
+        .exec();
+
+        if(!q)
+        { // Database returned no data
+            return res
+                .status(404)
+                .json({err});
+        } else { // Return resulting trip list
+            return res
+                .status(201)
+                .json(q);
+        }
+
+        // Uncomment the following line to show the results of operation
+        // on the console
+        // console.log(q);
+};
+
 module.exports = {
     tripsList,
-    tripsFindByCode
+    tripsFindByCode,
+    tripsAddTrip,
+    tripsUpdateTrip,
 };
